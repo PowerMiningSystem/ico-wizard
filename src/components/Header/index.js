d251ee85cb6d923e1abd48030682728f3bbfa21d
PowerMiningSystem <33825506+PowerMiningSystem@users.noreply.github.com>
2017-12-01 02:05:01 +0300

    Add files via upload

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import '../../assets/stylesheets/application.css';
 import { Link } from 'react-router-dom'
+import { setActiveLanguage } from 'react-localize-redux'
 
 export const Header = () => (
 	<header className="header">
-    <div className="container">
-      
+	<right>
+    <div className="container">  
+    const LanguageSelector = ({ languages, setActiveLanguage }) => (
+  <ul>
+    { languages.map(language => 
+      <li><button onClick={ setActiveLanguage }>{ language.code }</button></li>
+    )}
+  </ul>
+)   
     </div>
+    </right>
   </header>
 )
+const mapStateToProps = state => ({ languages: getLanguages(state.locale) });
+const mapDispatchToProps = { setActiveLanguage };
 
+export default connect(mapStateToProps, mapDispatchToProps)(LanguageSelector);
