7a7d75f38edb5282308ee1e383f3ec14d474b3f0
PowerMiningSystem <33825506+PowerMiningSystem@users.noreply.github.com>
2017-12-01 19:55:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,12 +15,6 @@ import { toast } from './utils/utils'
 import enLocale from 'react-intl/locale-data/en'
 import ruLocale from 'react-intl/locale-data/ru'
 
-addLocaleData([...enLocale, ...ruLocale]);
-
-const localeStore = new LocaleStore("en", translations);
-const store = {
-    locale: localeStore, 
-};
 
 class App extends Component {
   render() {
