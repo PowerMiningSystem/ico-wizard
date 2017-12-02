608265757e2ae577f4aea42f2bcfb33eb57ffd76
PowerMiningSystem <33825506+PowerMiningSystem@users.noreply.github.com>
2017-12-02 06:22:29 +0300

    Update alerts.js

--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -4,7 +4,7 @@ import 'sweetalert/dist/sweetalert.css';
 export function noMetaMaskAlert() {
     sweetAlert({
       title: "Warning",
-      text: "You don't have Metamask installed. Check instructions <a href='https://hackmd.io/s/SJSvCFJWz' target='blank'></a>.",
+      text: "You don't have Metamask installed. Check ICO Wizard GitHub for <a href='https://github.com/oraclesorg/ico-wizard' target='blank'>the instruction</a>.",
       html: true,
       type: "warning"
     });
@@ -57,3 +57,58 @@ export function successfulInvestmentAlert(tokensToInvest) {
     });
 }
 
+export function investmentDisabledAlert(startBlock, curBlock) {
+    sweetAlert({
+      title: "Warning",
+      text: "Wait, please. Crowdsale company hasn't started yet. It'll start from <b>" + startBlock + "</b> block. Current block is <b>" + curBlock + "</b>.",
+      html: true,
+      type: "warning"
+    });
+}
+
+export function investmentDisabledAlertInTime(startTime) {
+    sweetAlert({
+      title: "Warning",
+      text: "Wait, please. Crowdsale company hasn't started yet. It'll start from <b>" + new Date(startTime) + "</b>.",
+      html: true,
+      type: "warning"
+    });
+}
+
+export function incorrectNetworkAlert(correctNetworkName, incorrectNetworkName) {
+    sweetAlert({
+      title: "Warning",
+      text: "Crowdsale contract is from <b>" + correctNetworkName + " network</b>. But you are connected to <b>" + incorrectNetworkName + " network</b>. Please, change connection in MetaMask/Oracles plugin.",
+      html: true,
+      type: "warning"
+    });
+}
+
+export function noDeploymentOnMainnetAlert() {
+    sweetAlert({
+      title: "Warning",
+      text: "Wizard is under maintenance on Ethereum Mainnet. Please come back later or use Kovan/Rinkeby/Oracles. Follow <a href='https://twitter.com/oraclesorg'>https://twitter.com/oraclesorg</a> for status.",
+      html: true,
+      type: "warning"
+    });
+}
+
+export function warningOnMainnetAlert(tiersCount, cb) {
+  let n = 100 //fraction to round
+    let estimatedCost = 1.0 / n * Math.ceil(n * tiersCount * 0.16)
+    let estimatedTxsCount = tiersCount * 12
+    sweetAlert({
+      title: "Warning",
+      text: "You are about to sign " + estimatedTxsCount + " TXs. You will see an individual Metamask windows for each of it. Please don't open two or more instances of Wizard in one browser. ICO Wizard will create " + tiersCount + "-tier(s) crowdsale for you. The total cost will be around " + estimatedCost + " ETH. Are you sure you want to proceed?",
+      html: true,
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonText: 'Yes, I am sure!',
+      cancelButtonText: "No, cancel it!",
+    },
+    function(isConfirm) {
+        if (isConfirm) {
+          cb();
+        }
+    });
+}
