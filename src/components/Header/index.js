648af1270adb194969c5bc0d49b8c2277efcdcb5
PowerMiningSystem <33825506+PowerMiningSystem@users.noreply.github.com>
2017-12-02 06:57:29 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 
 export const Header = () => (
 	<header className="header">
+	<right>
     <div className="container">
-      
+      <a href="https://ico-ru.windminer.io">Russian version</a>
     </div>
+	</right>
   </header>
 )
 
